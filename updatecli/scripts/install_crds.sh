26a54e134eb093527352e0bc0c7d5413af7def1b
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-12-21 09:42:23 -0300

    wip

--- updatecli/scripts/install_crds.sh
+++ updatecli/scripts/install_crds.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 tar -xvf /tmp/crds.tar.gz
-ls /tmp
-echo "$PWD"
-ls
-echo "find . -maxdepth 1 -name \"*$1*\" -exec  mv \{\} charts/kubewarden-crds/templates/$1.yaml \;"
-find . -maxdepth 1 -name "*$1*" -exec  mv \{\} charts/kubewarden-crds/templates/$1.yaml \;
-#find . -maxdepth 1 -name "*policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyserver.yaml \;
-#find . -maxdepth 1 -name "*admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicy.yaml \;
-#find . -maxdepth 1 -name "*clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
+#ls /tmp
+#echo "$PWD"
+#ls
+#echo "find . -maxdepth 1 -name \"*$1*\" -exec  mv \{\} charts/kubewarden-crds/templates/$1.yaml \;"
+#find . -maxdepth 1 -name "*$1*" -exec  mv \{\} charts/kubewarden-crds/templates/$1.yaml \;
+
+find . -maxdepth 1 -name "*policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyservers.yaml \;
+find . -maxdepth 1 -name "*admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicies.yaml \;
+find . -maxdepth 1 -name "*clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
 
 # updatecli expects something in the stdout when a change happen.
 echo "Changed!"

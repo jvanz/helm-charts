c47ceb2663ecc6b5734eb7a128b880b20e208d16
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-12-21 08:49:54 -0300

    wip

--- updatecli/scripts/install_crds.sh
+++ updatecli/scripts/install_crds.sh
@@ -4,3 +4,6 @@ tar -xvf crds.tar.gz
 find . -maxdepth 1 -name "*policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyserver.yaml \;
 find . -maxdepth 1 -name "*admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicy.yaml \;
 find . -maxdepth 1 -name "*clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
+
+# updatecli expects something in the stdout when a change happen.
+echo "Changed!"

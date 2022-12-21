a8d24bab9a40a6facd9b6337e0d0a56e97f941f1
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-12-21 09:46:47 -0300

    wip

--- updatecli/scripts/install_crds.sh
+++ updatecli/scripts/install_crds.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-tar -xvf /tmp/crds.tar.gz
-#ls /tmp
-#echo "$PWD"
-#ls
-#echo "find . -maxdepth 1 -name \"*$1*\" -exec  mv \{\} charts/kubewarden-crds/templates/$1.yaml \;"
-#find . -maxdepth 1 -name "*$1*" -exec  mv \{\} charts/kubewarden-crds/templates/$1.yaml \;
-
+tar -xf /tmp/crds.tar.gz
 find . -maxdepth 1 -name "*policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyservers.yaml \;
 find . -maxdepth 1 -name "*admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicies.yaml \;
 find . -maxdepth 1 -name "*clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;

e92a95388e86867344a6ffdb2438579587f97395
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-12-21 08:59:35 -0300

    wip

--- updatecli/scripts/install_crds.sh
+++ updatecli/scripts/install_crds.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 tar -xvf crds.tar.gz
-find . -maxdepth 1 -name "*policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyserver.yaml \;
-find . -maxdepth 1 -name "*admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicy.yaml \;
-find . -maxdepth 1 -name "*clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
+find . -maxdepth 1 -name "*$1*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/$1.yaml \;
+#find . -maxdepth 1 -name "*policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyserver.yaml \;
+#find . -maxdepth 1 -name "*admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicy.yaml \;
+#find . -maxdepth 1 -name "*clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
 
 # updatecli expects something in the stdout when a change happen.
 echo "Changed!"
